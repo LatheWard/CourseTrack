617bebe587ad0ae9d4998a982fc936d51f6bdf22
Lathe Ward <lward21@basecampcodingacademy.org>
2021-04-26 11:26:41 -0500

    Need to decide what field to hold classes in

--- base/models.py
+++ base/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+# user model
+class The_User(AbstractUser):
+    coursesTaken = models.PositiveIntegerField
