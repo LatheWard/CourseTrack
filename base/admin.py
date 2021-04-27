80346bbea10517f19968684edfb2299e4b12ae68
Lathe Ward <lward21@basecampcodingacademy.org>
2021-04-27 08:25:33 -0500

    Added to admin page

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import The_User
+from .models import The_User, Course, UserGrade
 
 # Register your models here.
 admin.site.register(The_User)
+admin.site.register(Course)
+admin.site.register(UserGrade)
