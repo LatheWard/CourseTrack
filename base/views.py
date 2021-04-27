66fba6ee70313da5116ebfc002d9ec2f2ae82ed8
Lathe Ward <lward21@basecampcodingacademy.org>
2021-04-27 08:53:52 -0500

    Implementing views for creation, deletion, and details

--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
+from django.views.generic.edit import CreateView
+from .models import Course
 # Create your views here.
 
 class HomePageView(TemplateView):
@@ -7,3 +9,6 @@ class HomePageView(TemplateView):
 
 class HomePageView(TemplateView):
     homepage = 'home.html'
+
+class CourseDetailView(DetailView):
+    model = Course
