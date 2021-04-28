5405815e409091d6392456c2171aed8bd1b1d9c8
Lathe Ward <lward21@basecampcodingacademy.org>
2021-04-28 13:30:33 -0500

    Almost done with sub buttons and initiating manytomany

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from .views import HomePageView, CourseListView, CourseDetailView, SignUp
+from . import views
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
@@ -7,6 +8,5 @@ urlpatterns = [
     path('course/', CourseListView.as_view(), name='course_list'),
     path('course/<int:pk>', CourseDetailView.as_view(), name='course_detail'),
     path('signup/', SignUp.as_view(), name='signup'),
-  
-    
+    path("<instruction>/<pk>/", views.change_course, name='change-course'),    
 ]
